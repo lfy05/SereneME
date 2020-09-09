9db84460e9f7c3af585ae6a6942255cd94674e5e
Li Feiyang <lfy@onelittlegoal.com>
2020-09-09 13:52:25 -0400

    confetti on wear

--- wear/src/main/java/dev/feiyang/sereneme/MainActivity.java
+++ wear/src/main/java/dev/feiyang/sereneme/MainActivity.java
@@ -1,8 +1,14 @@
 package dev.feiyang.sereneme;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.view.View;
 import android.widget.TextView;
+
+import nl.dionsegijn.konfetti.KonfettiView;
+import nl.dionsegijn.konfetti.models.Shape;
+import nl.dionsegijn.konfetti.models.Size;
 
 public class MainActivity extends WearableActivity {
 
@@ -15,7 +21,21 @@ public class MainActivity extends WearableActivity {
 
         mTextView = (TextView) findViewById(R.id.text);
 
-        // Enables Always-on
-        setAmbientEnabled();
+        final KonfettiView konfettiView = findViewById(R.id.konfettiView);
+        konfettiView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                konfettiView.build()
+                        .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+                        .setDirection(0.0, 359.0)
+                        .setSpeed(1f, 5f)
+                        .setFadeOutEnabled(true)
+                        .setTimeToLive(2000L)
+                        .addShapes(Shape.Square.INSTANCE, Shape.Circle.INSTANCE)
+                        .addSizes(new Size(12, 5f))
+                        .setPosition(-50f, konfettiView.getWidth() + 50f, -50f, -50f)
+                        .streamFor(30, 5000L);
+            }
+        });
     }
 }
