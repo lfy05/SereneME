1493590de96a37fcedcb9b0b764bf23bbc3c4fbe
Li Feiyang <lfy@onelittlegoal.com>
2020-09-10 16:55:55 -0400

    Espresso UI test on time button clicks

--- mobile/src/androidTest/java/dev/feiyang/sereneme/ExampleInstrumentedTest.java
+++ mobile/src/androidTest/java/dev/feiyang/sereneme/ExampleInstrumentedTest.java
@@ -1,13 +1,36 @@
 package dev.feiyang.sereneme;
 
 import android.content.Context;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.ViewAssertion;
+import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.matcher.BoundedMatcher;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.regex.Matcher;
+
+import dev.feiyang.sereneme.Features.MainActivity;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -15,12 +38,33 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mainActivityActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("dev.feiyang.sereneme", appContext.getPackageName());
+    public void testMainActivity() {
+        onView(ViewMatchers.withId(R.id.time5)).perform(click());
+
+        onView(withId(R.id.time10)).check(matches(getVisibilityMatcher(View.VISIBLE)));
+    }
+
+    public static BoundedMatcher<View, View> getVisibilityMatcher(final int expectedVisibility){
+        return new BoundedMatcher<View, View>(View.class){
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Checking for visibility status");
+            }
+
+            @Override
+            protected boolean matchesSafely(View item) {
+                return item.getVisibility() == expectedVisibility;
+            }
+        };
     }
 }
+
