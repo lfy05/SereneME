55f137e596a889bd232833a5773f4c788a9d3be7
Li Feiyang <io@feiyang.dev>
2020-09-11 00:18:41 -0400

    Added graphing library

--- mobile/src/main/java/dev/feiyang/sereneme/Features/MeditateFragment.java
+++ mobile/src/main/java/dev/feiyang/sereneme/Features/MeditateFragment.java
@@ -20,11 +20,6 @@ import nl.dionsegijn.konfetti.KonfettiView;
 import nl.dionsegijn.konfetti.models.Shape;
 import nl.dionsegijn.konfetti.models.Size;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MeditateFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MeditateFragment extends Fragment{
 
     private HashMap<Integer, Button> timeButtons = new HashMap<Integer, Button>();
@@ -32,44 +27,9 @@ public class MeditateFragment extends Fragment{
     private KonfettiView mKonfettiView;
     private TextView mMsgOnKnob;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public MeditateFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MeditateFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MeditateFragment newInstance(String param1, String param2) {
-        MeditateFragment fragment = new MeditateFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
